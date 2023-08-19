8526924429fce0664e9a73284f5f1c5bb682abb4
Sana Mukhtar <130250734+sana-mukhtar@users.noreply.github.com>
2023-08-20 00:40:16 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,35 @@
+let string ="";
+let buttons = document.querySelectorAll(".btn");
+
+
+  Array.from(buttons).forEach(element => {
+    element.addEventListener("click" , (e)=>{
+
+    
+
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector(".input").value = string;
+        }
+
+      else  if(e.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector(".input").value = string;
+        }
+
+        else  if(e.target.innerHTML == 'C'){
+          string = string.substring(0 , string.length-1);
+          document.querySelector(".input").value = string;
+      }
+
+        else{
+        string = string + e.target.innerHTML;
+        document.querySelector("input").value = string;
+        } 
+    
+    })
+    
+ });
+
+
+
